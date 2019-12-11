55eb3dbdc254adc18a620dd597b28ad94129ad19
Lorkerty <lorkerty@gmail.com>
2019-12-11 23:40:04 +0200

    commit message

--- src/components/Creator.js
+++ src/components/Creator.js
@@ -1,17 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function Creator(props) {
-    const {creator} = props
-    //const body = <section>Footer</section> 
-    console.log(props, props.creator.img)
-   return(
-        <div className="card" width="200" height="auto">
-          <h2>{creator.name}</h2>                     
-          {/* { body }  */}
-          <img scr={creator.img} width = "300" height="300" alt={creator.name}/>
-          <h3 style={{color:"red"}}>
-          </h3>
-        </div>
-      );
-  }
-  export default Creator
+  const { creator } = props;
+  return (
+    <div className="card" width="200" height="auto">
+      <img src={creator.img} width="300" height="300" alt={creator.name} />
+    </div>
+  );
+}
+export default Creator;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
 import React from 'react'
 import {render} from 'react-dom'
-import Creator from './components/Creator'
-import creators from './fixtures.js'
 
-function App() {
-    return (
-       <div>
-           <h1>App Name</h1>
-           <Creator creator = {creators[0]}/>
-           <Creator creator = {creators[1]}/>
-           
-       </div>
-     );
-   }
-   export default App
-
+import App from './components/App'
 render(<App/> ,document.getElementById('root'))
